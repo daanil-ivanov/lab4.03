e0992f1fda46a9cbb5de79c17d0dc220a1718d52
danil <danil.ivanov@metalab.ifmo.ru>
2024-04-12 17:46:20 +0300

    Final version

--- intensity.py
+++ intensity.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
 from scipy.optimize import curve_fit
 
+
 def intensity(image, coord, radius):
     intensity_sum = 0
     pixel_count = 0
@@ -18,109 +19,78 @@ def intensity(image, coord, radius):
                 pixel_count += 1
     return intensity_sum / pixel_count
 
-def sinc(x, w):
-    return 0.76 * np.abs(np.sin(np.pi * x**2 * w) / (np.pi * x**2 * w))
 
-# Загрузка изображения
-# голый 1 -- зелёный
-# голый 2 -- жёлтый
-image = Image.open("/home/danil/PycharmProjects/lab4.03/photos/голый 2.tiff")
+def sinc(x, A, w):
+    return A * np.abs(np.sin(np.pi * x ** 2 * w) / (np.pi * x ** 2 * w))
 
-# Выбор центра окружности (x, y)
-# настраивается руками
-# жёлтый (1394, 661)
-# зелёный (1399, 671)
 
-center = (1394, 661)
+image = Image.open("/home/danil/PycharmProjects/lab4.03/photos/yellow.tiff")
 
-# просто в gnu imp открыл изображение, где нанёс размеры заранее
-# и посчитал расстояние в пикселях для нанесённого отрезка.
-pix_len = 1971-1644
-m_len = 3e+03 * 1e-6 # уже в метрах
+# выбор центра окружности (x, y), настраивается вручную
 
-l = m_len/pix_len # длина одного пикселя в метрах без фактора
+center = (591, 957)
 
-# так как при снятии лабы я криво указал длины (не тот размер щелей между ними оказался),
-# то я домножаю на фактор 1/3, так как похоже, что в 3 раза ошибаюсь
-l *= 1/3
+l = 2.1773  # мкм/пкс
+l = l * 1e-6
 
-# Задать отрезок для анализа в пикселях
-# ж 12, з 37
-X_d = range(12, center[0])
+# задать отрезок для анализа в пикселях
+X_d = range(center[0], 2015)
 n = len(X_d)
 R_d = np.zeros(n)
 for i in range(0, n):
-    R_d[i] = center[0] - X_d[i]
+    R_d[i] = abs(center[0] - X_d[i])
     R_d[i] = R_d[i] * l
 
-# Создаём массив под интенсивность в каждом пикселе по x в отрезке
+# создаём массив под интенсивность в каждом пикселе по x в отрезке
 I = np.zeros(n)
 
 # Вычисление интенсивности на отрезке X_d
 # Радиус -- радиус вокруг точки в пикселях для подсчёта интенсивности рядом, произвольная вещь
-# в целом не очень важна, хоть 3, хоть 5
-
-# но для лучшего сглаживания попросим побольше, поставил 21 для Ж. Слишком много тоже плохо.
-r = 21
+# но для лучшего сглаживания попросим побольше. Хотя слишком много тоже плохо.
+r = 20
 
 for i in range(0, n):
     I[i] = intensity(image, (X_d[i], center[1]), r)
 
-# получается, что к краям уходит ниже, чем в центре, что непорядок
-# скажем, что в центре условный ноль, а что ниже по модулю отразмим от нуля
-
-abs_null = min(I[-40:-1])
-for i in range(0, n):
-    I[i] = abs((I[i] - abs_null))
-
 plt.plot(R_d, I, c='r', lw=1.2, label='I (r)')
 
-
-
 # найдём все локальные экстремумы чуть дальше центра
-dist = 110
-I = np.array(list(reversed(I.tolist())))
-R_d = np.array(list(reversed(R_d.tolist())))
-# Находим локальные максимумы
+dist = 150
+I = np.array(list((I.tolist())))
+R_d = np.array(list((R_d.tolist())))
 peaks, _ = find_peaks(I[dist:])
-# Находим локальные минимумы
 min_peaks, _ = find_peaks(-I[dist:])
 
-# Отмечаем локальные максимумы и минимумы на графике
-plt.plot(R_d[dist:][peaks], I[dist:][peaks], 'ro', label='Локальные максимумы')
-plt.plot(R_d[dist:][min_peaks], I[dist:][min_peaks], 'go', label='Локальные минимумы')
+# отмечаем локальные максимумы и минимумы на графике
+plt.plot(R_d[dist:][peaks], I[dist:][peaks], 'bo')
+plt.plot(R_d[dist:][min_peaks], I[dist:][min_peaks], 'go')
 
-# plt.title(f"r = {r}")
 plt.legend(loc='best')
+plt.xlabel(f'r, м', fontsize=12)
+plt.ylabel(r'I, у.е.', fontsize=12)
 plt.show()
 
-# посчитаем функцию видности. Будем считать, что V(r), r -- радиус до максимума
+# посчитаем функцию видности
 n = min(len(peaks), len(min_peaks))
 peaks = peaks[:n]
 min_peaks = min_peaks[:n]
 V = np.zeros(n)
-# print(I[dist:][peaks])
-# print(I[dist:][min_peaks])
 for i in range(n):
     V[i] = (I[dist:][peaks][i] - I[dist:][min_peaks][i]) / (I[dist:][peaks][i] + I[dist:][min_peaks][i])
-    # print(V[i])
 
 plt.scatter(R_d[dist:][peaks], V, c='b', label='V')
 
+# аппроксимация данных кардинальным синусом
+popt, pcov = curve_fit(sinc, R_d[dist:][peaks], V, maxfev=2000000000, bounds=([0, -np.inf], [1, np.inf]))
+a, w = popt[0], popt[1]
 
-
-# Аппроксимация данных кардинальным синусом
-# тут берём первые n штук, далее, как по теории и как видно, видимость начинает обратно нарастать, что портит наш фит
-n = -1
-
-popt, pcov = curve_fit(sinc, R_d[dist:][peaks][:n], V[:n], maxfev=2000000000)#, bounds=([0, -np.inf], [1, np.inf]))
-print(np.sqrt(np.diag(np.abs(popt))))
-# Создание кривой аппроксимации
-x = np.linspace(R_d[dist:][peaks][:n][0], R_d[dist:][peaks][:n][-1], 5000)
+# создание кривой аппроксимации
+x = np.linspace(R_d[dist:][peaks][:n][0], R_d[dist:][peaks][:][-1], 5000)
 curve_fit = sinc(x, *popt)
-plt.plot(x, curve_fit, color='red', label='sinc')
+plt.plot(x, curve_fit, color='red', label=f'{a:.3f}*|sinc({w:.0f}*r^2)|')
 
-# эмпирически подобрали, что A = 0.87 для жёлтого
+plt.xlabel(f'r, м', fontsize=12)
+plt.ylabel(r'V, у.е.', fontsize=12)
 
 plt.legend(loc='best')
 plt.show()
--- rad.py
+++ rad.py
@@ -1,11 +1,14 @@
-import numpy as np
+from numpy import average
+
 
 def R(r, m, n):
-    return (r[m]**2 - r[n]**2)/((m-n)*r[-1])
+    return (r[m] ** 2 - r[n] ** 2) / ((m - n) * r[-1])
 
-# номера колец
+
+# номера колец (сдвинуты на -1 для удобства чтения и написания кода)
 n = [0, 1, 2, 3]
-# первые четыре соответствуют 4 кольцам, предпоследнее -- радиус размытости, последнее -- lambda
+
+# первые четыре числа соответствуют радиусам 4 колец, предпоследнее -- радиус размытости, последнее -- lambda
 # последняя буква названия переменной -- цвет
 rr = [726., 937., 1100., 1243., 3033., 630.]
 ro = [671., 880., 1040., 1180., 2997., 578.]
@@ -13,24 +16,27 @@ rg = [651., 852., 1010., 1143., 2883., 546.]
 rb = [575., 780., 933., 1073., 2843., 435.]
 colors = [rr, ro, rg, rb]
 R_c = [[] for _ in range(4)]
+
 # переведём всё в метры
 for i, r in enumerate(colors):
     for j, rr in enumerate(r):
-        if j == len(r)-1:
+        if j == len(r) - 1:
             colors[i][j] *= 1e-9
         else:
             colors[i][j] *= 1e-6
 
+# посчитаем по формуле
 for i, r in enumerate(colors):
     for N in n:
         for M in n:
             if M > N:
-                 R_c[i].append(R(r, M, N))
+                R_c[i].append(R(r, M, N))
+
 blur_rad = 0
 for i in range(len(colors)):
-    R_c[i] = np.average(R_c[i])
+    R_c[i] = average(R_c[i])
     blur_rad += colors[i][-2]
-    print(f"{R_c[i]:.3f} м")
 
+print(f"радиус кривизны линзы {average(R_c):.3f} м")
 blur_rad /= 4
-print(f"{blur_rad * 1e6:.0f} мкм")
+print(f"радиус размытости {blur_rad * 1e6:.0f} мкм")
